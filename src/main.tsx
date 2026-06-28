c374c6f3054590566f0e8aa770bd5cf64d928a4b
h9k99rpg8n-cloud <h9k99rpg8n@privaterelay.appleid.com>
2026-06-28 16:40:46 -0600

    Make React entrypoint TypeScript-friendly

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { App } from './app/App'
 import './styles/globals.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Root element not found')
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
